71e79b97231179fb2043935866fd55a28450887a
Maeve Crossan <maevecrossan@gmail.com>
2026-01-29 12:31:52 +0000

    Update styles to allow focusboard to expand.

--- cosy-focus/app/page.tsx
+++ cosy-focus/app/page.tsx
@@ -3,7 +3,7 @@ import FocusBoard from "../components/focus/FocusBoard";
 
 export default function Home() {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full flex items-center justify-center bg-white">
       <main>
         <FocusBoard />
       </main>
