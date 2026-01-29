85903df1603f477dd8566d232c1b61d25414aff6
Maeve Crossan <maevecrossan@gmail.com>
2026-01-29 12:15:02 +0000

    Remove extra styles. Import FocusBoard and confirm working.

--- cosy-focus/app/page.tsx
+++ cosy-focus/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
+import FocusBoard from "../components/focus/FocusBoard";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="text-pink-600 font-bold text-xl mt-8 text-center">
-          Tailwind is working
-        </div>
+    <div className="w-full h-full">
+      <main>
+        <FocusBoard />
       </main>
     </div>
   );
