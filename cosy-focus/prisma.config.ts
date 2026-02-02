a0a11cb5443f28b1a67be56a9577cd607c874052
Maeve Crossan <maevecrossan@gmail.com>
2026-02-02 08:34:12 +0000

    Add datasource URL to prisma.config.ts.

--- cosy-focus/prisma.config.ts
+++ cosy-focus/prisma.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
   migrations: {
     path: "prisma/migrations",
     seed: "dotenv -e .env -- tsx prisma/seed.ts",
