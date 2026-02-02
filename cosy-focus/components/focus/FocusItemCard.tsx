0987ddebd1649ca057cb0d52ed67b90b8566d5c5
Maeve Crossan <maevecrossan@gmail.com>
2026-02-02 09:07:35 +0000

    Import button component. Create 'start focus' and 'complete' button. Both button confirmed showing up under each focus item.

--- cosy-focus/components/focus/FocusItemCard.tsx
+++ cosy-focus/components/focus/FocusItemCard.tsx
@@ -1,16 +1,27 @@
 // Card component to display a focus item with title, description and status
 
 import { FocusItem } from './Column';
+import Button from '../ui/Button';
 
 type FocusItemCardProps = {
     item: FocusItem;
 };
 
-export default function FocusItemCard( { item }: FocusItemCardProps) {
+export default function FocusItemCard({ item }: FocusItemCardProps) {
     return (
-        <div className='rounded-xl border bg-white p-3'>
-            <p className="font-medium">{item.title}</p>
-            <p className="text-xs opacity-60">#{item.id}</p>
-        </div>
+        <section>
+            <div className='rounded-xl border bg-white p-3'>
+                <p className="font-medium">{item.title}</p>
+                <p className="text-xs opacity-60">#{item.id}</p>
+            </div>
+
+            <Button className="mt-2 w-full" variant="primary">
+                Start Focus
+            </Button>
+
+            <Button className="mt-2 w-full" variant="secondary">
+                Complete
+            </Button>
+        </section>
     );
 }
